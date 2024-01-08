aa450990e69f6b1637d108a7910fa8ef2df914ac
matteo-didone <matteo.dido9@gmail.com>
2024-01-08 12:40:08 +0100

    Added launch.json file with right configuration

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Come ti chiami?");
-// Creo una variabile name che prende in input una stringa dell'utente
-var name = Console.ReadLine();
-// Stampo un messaggio che saluta l'utente 
-Console.WriteLine("Ciao, " + name + "! Benvenuto");
+
+//Enumeratore: tipo di dato che contiene dei valori fissi
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Clear(); 
+
+// Ciclo for
+for (int i = 0; i < 10; i++) 
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("END"); // Viene invocato il metodo toString() che converte quello che scrivo in una vera e propria stringa
+
+var x = Console.ReadLine(); // Il ReadLine ci lascia scrivere, e rimane fermo fin tanto che non premiamo invio
+var k = Console.ReadKey(); // Il ReadKey ci lascia premere solo un tasto, e non appena lo premiamo il codice prosegue
+if(k.Key == ConsoleKey.Enter) 
+{
+    Console.WriteLine("Hai premuto invio");
+}
+else if (k.KeyChar == 'a') 
+{
+    Console.WriteLine("Hai premuto a");
+}
+
+// Quando compiliamo codice, siamo in modalità "debug"
+
 
 // Per avviare il progetto: dotnet run
 // Per compilare il progetto: dotnet build
--- obj/Debug/net8.0/SampleCore1.AssemblyInfo.cs
+++ obj/Debug/net8.0/SampleCore1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleCore1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df028804efa7b7ec602b5f9e945780294aaa222f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58a30bb774bfe378dd6256185dbee6bbda688285")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleCore1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleCore1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
