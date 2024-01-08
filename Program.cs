419e1499f870e2e5a8179575c1f24501af316cf7
matteo-didone <matteo.dido9@gmail.com>
2024-01-08 11:04:22 +0100

    Added input from user and basic greeting message

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Come ti chiami?");
+// Creo una variabile name che prende in input una stringa dell'utente
+var name = Console.ReadLine();
+// Stampo un messaggio che saluta l'utente 
+Console.WriteLine("Ciao, " + name + "! Benvenuto");
+
 
 // Per avviare il progetto: dotnet run
 // Per compilare il progetto: dotnet build
--- obj/Debug/net8.0/SampleCore1.AssemblyInfo.cs
+++ obj/Debug/net8.0/SampleCore1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleCore1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6d4779d6b6266a5f64bc752016db1cf34a70d48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df028804efa7b7ec602b5f9e945780294aaa222f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleCore1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleCore1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
