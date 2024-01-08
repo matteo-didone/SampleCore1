df028804efa7b7ec602b5f9e945780294aaa222f
matteo-didone <matteo.dido9@gmail.com>
2024-01-08 11:00:32 +0100

    Ran dotnet build

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Per avviare il progetto: dotnet run
+// Per compilare il progetto: dotnet build
